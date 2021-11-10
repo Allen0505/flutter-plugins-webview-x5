f39bbef2698bfb2019e3df480fc160ca5a500bd8
wanghusheng.whs@bytedance.com <wanghusheng.whs@bytedance.com>
2021-11-10 16:16:37 +0800

    离线资源 fixbugs FLTWebViewFlutterPlugin Duplicate

--- packages/webview_flutter/ios/Classes/FLTWebViewFlutterPlugin.m
+++ packages/webview_flutter/ios/Classes/FLTWebViewFlutterPlugin.m
@@ -2,16 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "FLTWebViewFlutterPlugin.h"
+
+#import "WebViewFlutterPlugin.h"
 #import "FLTCookieManager.h"
 #import "FlutterWebView.h"
-
+#import "FlutterNSURLProtocol.h"
+#import "NSURLProtocol+WKWebView.h"
 @implementation FLTWebViewFlutterPlugin
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FLTWebViewFactory* webviewFactory =
       [[FLTWebViewFactory alloc] initWithMessenger:registrar.messenger];
   [registrar registerViewFactory:webviewFactory withId:@"plugins.flutter.io/webview"];
+    [NSURLProtocol registerClass:[FlutterNSURLProtocol class]];
+    [NSURLProtocol wk_registerScheme:@"http"];
+    [NSURLProtocol wk_registerScheme:@"https"];
   [FLTCookieManager registerWithRegistrar:registrar];
 }
 
